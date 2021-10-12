09e9f1be45a0c0260eb4680ad4d7ae32b6f9ccea
AnggiAprianda <anggiaprianda2000@gmail.com>
2021-10-12 13:22:17 +0700

    Commit Pertama

---
+++ main.java
@@ -0,0 +1,35 @@
+
+public class main {
+    public static void main(String[] args) {
+
+      String firstName;
+      String lastName;
+      String addreas;
+      String hoby;
+      String phoneNumber;
+      String makanan;
+      int Umur;
+
+      firstName = "Anggi";
+      lastName = "Aprianda";
+      addreas = "Tiban Indah Sekupang";
+      hoby = "Ohlaraga";
+      makanan = "Mie Ayam";
+      phoneNumber ="0852 6331 3510";
+      Umur = 21 ;
+ 
+      System.out.println("Nama : " + firstName + lastName);   
+      System.out.println("Alamat : " + addreas);
+      System.out.println("Umur : " + Umur);
+      System.out.println("hobi : " + hoby);
+      System.out.println("Telepon : " + phoneNumber);
+      System.out.println("Makanan Kesukaan: " + makanan);
+      
+    }
+
+    {
+
+  
+    }
+  }
+  
